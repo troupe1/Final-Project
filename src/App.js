ec8683c3ced9ad61792b10a7b59c835a8cdca22f
nicole <scsfmac11@Admins-MacBook-Air.local>
2018-05-24 17:57:04 -0700

    final changes

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import Game from './Game';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Game />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/commons/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => (
+  <nav className='navbar navbar-light bg-light'>
+    <p className="navbar-brand">Navbar</p>
+   </nav>
+ )
+
+export default Header
